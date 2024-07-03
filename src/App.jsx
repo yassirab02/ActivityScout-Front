8d5ff2bdc055ee88ad286de379aabfe98873a6e1
yassirab02 <yassirabourial86@gmail.com>
2024-07-03 19:00:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='font-serif text-xl text-red-800 bg-green-700'>Hello</h1>
+    </>
+  )
+}
+
+export default App
